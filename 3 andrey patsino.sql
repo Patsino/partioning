54535364f2826417622095e1601f63fcf0405898
Patsino <89965214+Patsino@users.noreply.github.com>
2024-05-26 23:44:50 +0300

    Add files via upload

---
+++ 3 andrey patsino.sql
@@ -0,0 +1,14 @@
+SELECT * FROM sales_data 
+WHERE sale_date >= '2022-06-03' AND sale_date < '2023-06-03';
+
+SELECT to_char(sale_date, 'YYYY-MM') AS month, SUM(sale_amount) AS total_sale_amount
+FROM sales_data
+GROUP BY to_char(sale_date, 'YYYY-MM')
+ORDER BY month;
+
+SELECT salesperson_id, SUM(sale_amount) AS total_sales
+FROM sales_data
+WHERE region_id = 5
+GROUP BY salesperson_id
+ORDER BY total_sales DESC
+LIMIT 3;
